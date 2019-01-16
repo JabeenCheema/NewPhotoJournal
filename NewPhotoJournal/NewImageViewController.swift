eeada9462387529c569146bbc2d20d29230880c1
Jabeen Cheema <jabeencheema@pursuit.org>
2019-01-16 17:27:15 -0500

    first commit

---
+++ NewPhotoJournal/NewImageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewImageViewController.swift
+//  NewPhotoJournal
+//
+//  Created by Jabeen's MacBook on 1/14/19.
+//  Copyright © 2019 Jabeen's MacBook. All rights reserved.
+//
+
+import UIKit
+
+class NewImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
